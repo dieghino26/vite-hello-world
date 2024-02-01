683ea720b2ede0154e1dd68bfe082e11147e75b8
dieghino26 <brotzadiego@gmail.com>
2024-02-01 09:58:09 +0100

    added AppTitle in App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup>
-
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  name: "Vue hello",
+  components: { AppTitle }
+};
 </script>
 
-<template></template>
+<template>
+  <AppTitle />
+</template>
 
 <style scoped></style>
